bae4cd7cf78c5b04cca52de8e53b908578588f31
Alpen Ditrix <avadend.piroserpen@gmail.com>
2019-04-19 17:59:24 +0300

    Rewritten with minimal repro case

--- Main.java
+++ Main.java
@@ -1,26 +1,5 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
 public class Main
 {
-
-    private static OutputStream out;
-
-    static
-    {
-        try
-        {
-            out = new FileOutputStream("test.ttf");
-        }
-        catch (FileNotFoundException e)
-        {
-            System.out.println("Exception in file opening");
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args)
     {
         System.out.println("Checking fonts availability");
@@ -47,18 +26,6 @@ public class Main
         } finally
         {
             System.out.println("Test finished");
-        }
-    }
-
-    private static void write(int i) {
-        try
-        {
-            out.write(i);
-        }
-        catch (IOException e)
-        {
-            System.out.println("Exception in .write()");
-            e.printStackTrace();
         }
     }
 }
